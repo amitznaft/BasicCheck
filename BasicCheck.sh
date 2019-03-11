c7e6732103c619f089ea2ce5ae922b1f01d0940b
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-11 14:21:59 +0200

    Add files via upload

---
+++ BasicCheck.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+folderName=$1
+executeble=$2
+curentLocation=`pwd`
+
+cd $folderName
+make > /dev/null
+cheakVal=$?
+if [[ cheakVal -gt '0' ]]; then
+        echo "Compilation Error"
+        exit 7
+else
+        echo "Compilation Pass"
+fi
+
+valgrind --log-file=/dev/null ./$executeble $@
+valout=$?
+if [[ valout -eq '0' ]]; then
+        leaks=0
+else
+        leaks=1
+
+fi
+
+valgrind --tool=helgrind --log-file=/dev/null ./$executeble
+threads=$?
+
+if [[ threads -eq '0' ]]; then
+        isthreads=0
+else
+        isthreads=1
+fi
+strCat=$leaks$isthreads
+if [[ $strCat == '11' ]]; then
+        echo "Memory leaks:FAIL, thread race: FAIL"
+        exit 3
+elif [[ $strCat == '01' ]]; then
+        echo "Memory leaks:PASS, thread race:FAIL"
+elif [[ $strCat == '10' ]]; then
+        echo "Memory leaks:FAIL , thred race : PASS"
+        exit 2
+else
+        echo "Memory leaks :PASS , thred race :PASS"
+        exit 0
+fi
