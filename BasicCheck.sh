ad1cd9d066db2db490a1c991f4a735e296db4e9a
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-12 17:37:10 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -15,31 +15,19 @@ fi
 
 valgrind —-leak-check=full —-error-exitcode=1 ./$executeble $@ &> /dev/null
 valout=$?
-if [ valout -eq 0 ]; then
-        leaks=0
-else
-        leaks=1
-
-fi
 
 valgrind --tool=helgrind —-error-exitcode=1 --log-file=/dev/null ./$executeble
 threads=$?
 
-if [ threads -eq 0 ]; then
-        isthreads=0
-else
-        isthreads=1
-fi
-
-if [ leaks -eq 1 ] && [ isthreads -eq 1 ]; then
+if [ valout -eq 1 ] && [ threads -eq 1 ]; then
         echo "Memory leaks:FAIL, thread race: FAIL"
 	cd $currentLocation
         exit 3
-elif  [ leaks -eq 0 ] && [ isthreads -eq 1 ]; then
+elif  [ valout -eq 0 ] && [ threads -eq 1 ]; then
         echo "Memory leaks:PASS, thread race:FAIL"
 	cd $currentLocation
 	exit 1
-elif  [ leaks -eq 1 ] && [ isthreads -eq 0 ]; then
+elif  [ valout -eq 1 ] && [ threads -eq 0 ]; then
         echo "Memory leaks:FAIL , thred race : PASS"
 	cd $currentLocation
         exit 2
