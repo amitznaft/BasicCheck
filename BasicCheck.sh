d076205f49a9a3e94c43fcaf98afedd291c509ff
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-12 19:25:06 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -6,7 +6,7 @@ curentLocation=`pwd`
 cd $folderName
 make > /dev/null
 cheakVal=$?
-if [[ $cheakVal -gt 0 ]]; then
+if [ “$cheakVal” -gt “0” ]; then
         echo "Compilation Error"
         exit 7
 else
@@ -15,22 +15,31 @@ fi
 
 valgrind —-leak-check=full --error-exitcode=1 ./$executeble $@ &> /dev/null
 valout=$?
+if [ “$valout” -eq “0”]; then
+	er=0
+else 
+	er=1
+fi
 
 valgrind --tool=helgrind —-error-exitcode=1 ./$executeble $@ &> /dev/null
 threads=$?
+if [ “$threads” -eq “0”]; then
+	et=0
+else 
+	et=1
+fi
 
-echo $valout
-echo $threads
 
-if [[ $valout -eq 1 ]] && [[ $threads -eq 1 ]]; then
+
+if [[ “$er -eq “1” ]] && [[ “$et” -eq “1” ]]; then
         echo "Memory leaks:FAIL, thread race: FAIL"
 	cd $currentLocation
         exit 3
-elif [[ $valout -eq 0 ]] && [[ $threads -eq 1 ]]; then
+elif [[ “$er -eq “0” ]] && [[ “$et” -eq “1” ]]; then
         echo "Memory leaks:PASS, thread race:FAIL"
 	cd $currentLocation
 	exit 1
-elif [[ $valout -eq 1 ]] && [[ $threads -eq 0 ]]; then
+elif [[ “$er -eq “1” ]] && [[ “$et” -eq “0” ]]; then
         echo "Memory leaks:FAIL , thred race : PASS"
 	cd $currentLocation
         exit 2
