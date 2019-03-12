781644fba53accc9e53f9509532b8f342a5e71ce
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-12 17:35:23 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -6,7 +6,7 @@ curentLocation=`pwd`
 cd $folderName
 make > /dev/null
 cheakVal=$?
-if [[ $cheakVal -gt 0 ]]; then
+if [ cheakVal -gt 0 ]; then
         echo "Compilation Error"
         exit 7
 else
@@ -15,7 +15,7 @@ fi
 
 valgrind —-leak-check=full —-error-exitcode=1 ./$executeble $@ &> /dev/null
 valout=$?
-if [[ $valout -eq 0 ]]; then
+if [ valout -eq 0 ]; then
         leaks=0
 else
         leaks=1
@@ -25,21 +25,21 @@ fi
 valgrind --tool=helgrind —-error-exitcode=1 --log-file=/dev/null ./$executeble
 threads=$?
 
-if [[ $threads -eq 0 ]]; then
+if [ threads -eq 0 ]; then
         isthreads=0
 else
         isthreads=1
 fi
 
-if [[ $leaks -eq 1 and $isthreads -eq 1 ]]; then
+if [ leaks -eq 1 ] && [ isthreads -eq 1 ]; then
         echo "Memory leaks:FAIL, thread race: FAIL"
 	cd $currentLocation
         exit 3
-elif [[ $leaks -eq 0 and $isthreads -eq 1 ]]; then
+elif  [ leaks -eq 0 ] && [ isthreads -eq 1 ]; then
         echo "Memory leaks:PASS, thread race:FAIL"
 	cd $currentLocation
 	exit 1
-elif [[ $leaks -eq 1 and $isthreads -eq 0 ]]; then
+elif  [ leaks -eq 1 ] && [ isthreads -eq 0 ]; then
         echo "Memory leaks:FAIL , thred race : PASS"
 	cd $currentLocation
         exit 2
