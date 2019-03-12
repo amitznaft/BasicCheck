52851af3ccd043cff803454e472214f1793c106c
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-13 01:06:22 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,45 +1,103 @@
 #!/bin/bash
+
 folderName=$1
-executable=$2
-currentLocation=`pwd`
+
+executeble=$2
+
 shift 2
+
+curentLocation=`pwd`
+
 cd $folderName
-make
-isMake=$?
-if [ "$isMake" -gt "0" ] ; then
-        echo "Compilation FAIL"
+
+make > /dev/null
+
+cheakVal=$?
+
+if [ “$cheakVal” -gt “0” ] ; then
+
+        echo "Compilation Error"
+
         exit 7
-fi
 
-
-valgrind --leak-check=full --error-exitcode=1 ./$executable $@ &> /dev/null
-valgrindgout=$?
-if [ "$valgrindgout" -eq "0" ] ; then
-        error=0
 else
-        error=1
-fi
 
-valgrind --tool=helgrind --error-exitcode=1 ./$executable $@ &> /dev/null
-race=$?
-if [ "$race" -eq "0" ] ; then
-        isRace=0
-else
-        isRace=1
+        echo "Compilation Pass"
+
 fi
 
 
-status=$error$isRace
-if [ "$status" -eq "00" ] ; then
-        echo "Compilation PASS  Memory leaks PASS       Tread race PASS"
+
+
+valgrind —-leak-check=full --error-exitcode=1 ./$executeble $@ &> /dev/null
+
+valout=$?
+
+if [ “$valout” -eq “0” ] ; then
+
+	er=0
+
+else 
+
+	er=1
+
+fi
+
+
+
+
+valgrind --tool=helgrind —-error-exitcode=1 ./$executeble $@ &> /dev/null
+
+threads=$?
+
+if [ “$threads” -eq “0” ] ; then
+
+	et=0
+
+else 
+
+	et=1
+
+fi
+
+
+
+
+
+
+
+valthr=$er$et
+
+if [ "$valthr" -eq “00” ] ; then
+
+        echo "Compilation PASS , Memory leaks :PASS , thred race :PASS"
+
+	cd $currentLocation
+
         exit 0
-elif [ "$status" -eq "10" ] ; then
-        echo "Compilation PASS  Memory leaks FAIL       Tread race PASS"
-        exit 2
-elif [ "$status" -eq "01" ] ; then
-        echo "Compilation PASS  Memory leaks PASS       Tread race FAIL"
+
+elif [ "$valthr" -eq “10” ] ; then
+
+        echo "Compilation PASS , Memory leaks:FAIL , thred race : PASS"
+
+	cd $currentLocation
+
+	exit 2
+
+elif [ "$valthr" -eq “01” ] ; then
+
+        echo "Compilation PASS , Memory leaks:PASS , thred race : FAIL”
+
+	cd $currentLocation
+
         exit 1
+
 else
-        echo "Compilation PASS  Memory leaks FAIL       Tread race FAIL"
+
+        echo "Compilation PASS , Memory leaks :FAIL , thred race :FAIL”
+
+	cd $currentLocation
+
         exit 3
+
 fi
