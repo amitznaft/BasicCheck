8d6cfb8ab51643353073204037ead3fa9ccb278f
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-13 01:15:45 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,103 +1,44 @@
 #!/bin/bash
-
 folderName=$1
-
 executeble=$2
-
-shift 2
-
 curentLocation=`pwd`
-
+shift 2
 cd $folderName
-
-make > /dev/null
-
+make 
 cheakVal=$?
-
 if [ “$cheakVal” -gt “0” ] ; then
-
         echo "Compilation Error"
-
-        exit 7
-
-else
-
-        echo "Compilation Pass"
-
+        exit 7	
 fi
-
-
-
 
 valgrind —-leak-check=full --error-exitcode=1 ./$executeble $@ &> /dev/null
-
 valout=$?
-
 if [ “$valout” -eq “0” ] ; then
-
 	er=0
-
 else 
-
 	er=1
-
 fi
-
-
-
 
 valgrind --tool=helgrind —-error-exitcode=1 ./$executeble $@ &> /dev/null
-
 threads=$?
-
 if [ “$threads” -eq “0” ] ; then
-
 	et=0
-
 else 
-
 	et=1
-
 fi
-
-
-
-
-
 
 
 valthr=$er$et
-
 if [ "$valthr" -eq “00” ] ; then
-
         echo "Compilation PASS , Memory leaks :PASS , thred race :PASS"
-
-	cd $currentLocation
-
         exit 0
-
 elif [ "$valthr" -eq “10” ] ; then
-
         echo "Compilation PASS , Memory leaks:FAIL , thred race : PASS"
-
-	cd $currentLocation
-
 	exit 2
-
 elif [ "$valthr" -eq “01” ] ; then
-
         echo "Compilation PASS , Memory leaks:PASS , thred race : FAIL”
-
-	cd $currentLocation
-
         exit 1
-
 else
-
         echo "Compilation PASS , Memory leaks :FAIL , thred race :FAIL”
-
-	cd $currentLocation
-
         exit 3
-
 fi
