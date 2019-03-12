f5d4a2a5d2af52a240ed1185d25224086dd5c104
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-12 13:35:04 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -5,41 +5,41 @@ curentLocation=`pwd`
 
 cd $folderName
 make > /dev/null
-cheakVal=$?
-if [[ cheakVal -gt '0' ]]; then
-        echo "Compilation Error"
+isfullMake=$?
+if [[ isfullMake -gt '0' ]]; then
+        echo "Compilation Fail"
         exit 7
 else
         echo "Compilation Pass"
 fi
 
-valgrind —error-exitcode=1 —-leak-check=full-v ./$executeble $@
-valout=$?
-if [[ valout -eq '0' ]]; then
+valgrind --log-file=/dev/null ./$executeble $@
+valgridout=$?
+if [[ valgridout -eq '0' ]]; then
         leaks=0
 else
         leaks=1
 
 fi
 
-valgrind —error-exitcode=1 —tool=helgrind ./$executeble
-threads=$?
+valgrind --tool=helgrind --log-file=/dev/null ./$executeble
+thredrace=$?
 
-if [[ threads -eq '0' ]]; then
-        isthreads=0
+if [[ thredrace -eq '0' ]]; then
+        isthredrace=0
 else
-        isthreads=1
+        isthredrace=1
 fi
-strCat=$leaks$isthreads
-if [[ $strCat == '11' ]]; then
-        echo "Memory leaks:FAIL, thread race: FAIL"
-        exit 3
-elif [[ $strCat == '01' ]]; then
+result=$leaks$isthredrace
+if [[ $result == '00' ]]; then
+        echo "Memory leaks:PASS, thread race: PASS"
+        exit 0
+elif [[ $result == '01' ]]; then
         echo "Memory leaks:PASS, thread race:FAIL"
-elif [[ $strCat == '10' ]]; then
+elif [[ $result == '10' ]]; then
         echo "Memory leaks:FAIL , thred race : PASS"
         exit 2
 else
-        echo "Memory leaks :PASS , thred race :PASS"
-        exit 0
+        echo "Memory leaks :FAIL , thred race :FAIL"
+        exit 3
 fi
