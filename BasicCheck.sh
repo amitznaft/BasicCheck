d4760f71d05ac6aed6e9f77472390fa83bc4909e
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-13 01:30:39 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 folderName=$1
-executeble=$2
-curentLocation=`pwd`
+executable=$2
+currentLocation=`pwd`
 shift 2
 cd $folderName
 make 
-cheakVal=$?
-if [ “$cheakVal” -gt “0” ] ; then
+checkMake=$?
+if [ “$checkMake” -gt “0” ] ; then
         echo "Compilation Error"
         exit 7	
 fi
 
-valgrind —-leak-check=full --error-exitcode=1 ./$executeble $@ &> /dev/null
+valgrind —-leak-check=full --error-exitcode=1 ./$executable $@ &> /dev/null
 valout=$?
 if [ “$valout” -eq “0” ] ; then
-	er=0
+	errorval=0
 else 
-	er=1
+	errorval=1
 fi
 
-valgrind --tool=helgrind —-error-exitcode=1 ./$executeble $@ &> /dev/null
+valgrind --tool=helgrind —-error-exitcode=1 ./$executable $@ &> /dev/null
 threads=$?
 if [ “$threads” -eq “0” ] ; then
-	et=0
+	errorHel=0
 else 
-	et=1
+	errorHel=1
 fi
 
 
-valthr=$er$et
-if [ "$valthr" -eq “00” ] ; then
+check=$errorval$errorHel
+if [ "$check" -eq “00” ] ; then
         echo "Compilation PASS , Memory leaks :PASS , thred race :PASS"
         exit 0
-elif [ "$valthr" -eq “10” ] ; then
+elif [ "$check" -eq “10” ] ; then
         echo "Compilation PASS , Memory leaks:FAIL , thred race : PASS"
 	exit 2
-elif [ "$valthr" -eq “01” ] ; then
+elif [ "$check" -eq “01” ] ; then
         echo "Compilation PASS , Memory leaks:PASS , thred race : FAIL”
         exit 1
 else
