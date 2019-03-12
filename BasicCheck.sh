33a667932283cf8726ec0461ef754c3521a280cf
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-13 01:37:59 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -4,41 +4,41 @@ executable=$2
 currentLocation=`pwd`
 shift 2
 cd $folderName
-make 
-checkMake=$?
-if [ “$checkMake” -gt 0 ] ; then
-        echo "Compilation Error"
-        exit 7	
+make
+isMake=$?
+if [ "$isMake" -gt "0" ] ; then
+        echo "Compilation FAIL"
+        exit 7
 fi
 
-valgrind —-leak-check=full --error-exitcode=1 ./$executable $@ &> /dev/null
-valout=$?
-if [ “$valout” -eq “0” ] ; then
-	errorval=0
-else 
-	errorval=1
+valgrind --leak-check=full --error-exitcode=1 ./$executable $@ &> /dev/null
+valgrindgout=$?
+if [ "$valgrindgout" -eq "0" ] ; then
+        error=0
+else
+        error=1
 fi
 
-valgrind --tool=helgrind —-error-exitcode=1 ./$executable $@ &> /dev/null
-threads=$?
-if [ “$threads” -eq “0” ] ; then
-	errorHel=0
-else 
-	errorHel=1
+valgrind --tool=helgrind --error-exitcode=1 ./$executable $@ &> /dev/null
+race=$?
+if [ "$race" -eq "0" ] ; then
+        isRace=0
+else
+        isRace=1
 fi
 
 
-check=$errorval$errorHel
-if [ "$check" -eq “00” ] ; then
-        echo "Compilation PASS , Memory leaks :PASS , thred race :PASS"
+status=$error$isRace
+if [ "$status" -eq "00" ] ; then
+        echo "Compilation PASS  Memory leaks PASS       Tread race PASS"
         exit 0
-elif [ "$check" -eq “10” ] ; then
-        echo "Compilation PASS , Memory leaks:FAIL , thred race : PASS"
-	exit 2
-elif [ "$check" -eq “01” ] ; then
-        echo "Compilation PASS , Memory leaks:PASS , thred race : FAIL”
+elif [ "$status" -eq "10" ] ; then
+        echo "Compilation PASS  Memory leaks FAIL       Tread race PASS"
+        exit 2
+elif [ "$status" -eq "01" ] ; then
+        echo "Compilation PASS  Memory leaks PASS       Tread race FAIL"
         exit 1
 else
-        echo "Compilation PASS , Memory leaks :FAIL , thred race :FAIL”
+        echo "Compilation PASS  Memory leaks FAIL       Tread race FAIL"
         exit 3
 fi
