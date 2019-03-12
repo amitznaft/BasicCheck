b87ec86e1542da918abc18050c38a1eacad2e66e
amitznaft <44845542+amitznaft@users.noreply.github.com>
2019-03-12 19:51:56 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -15,7 +15,7 @@ fi
 
 valgrind —-leak-check=full --error-exitcode=1 ./$executeble $@ &> /dev/null
 valout=$?
-if [ “$valout” -eq “0”]; then
+if [ “$valout” -eq “0” ] ; then
 	er=0
 else 
 	er=1
@@ -23,28 +23,28 @@ fi
 
 valgrind --tool=helgrind —-error-exitcode=1 ./$executeble $@ &> /dev/null
 threads=$?
-if [ “$threads” -eq “0”]; then
+if [ “$threads” -eq “0” ] ; then
 	et=0
 else 
 	et=1
 fi
 
 
-
-if [[ “$er -eq “1” ]] && [[ “$et” -eq “1” ]]; then
-        echo "Memory leaks:FAIL, thread race: FAIL"
+valthr=$er$et
+if [ "$valthr" -eq "11" ] ; then
+        echo "Compilation PASS , Memory leaks:FAIL, thread race: FAIL"
 	cd $currentLocation
         exit 3
-elif [[ “$er -eq “0” ]] && [[ “$et” -eq “1” ]]; then
-        echo "Memory leaks:PASS, thread race:FAIL"
+elif [ "$valthr" -eq "01" ] ; then
+        echo "Compilation PASS , Memory leaks:PASS, thread race:FAIL"
 	cd $currentLocation
 	exit 1
-elif [[ “$er -eq “1” ]] && [[ “$et” -eq “0” ]]; then
-        echo "Memory leaks:FAIL , thred race : PASS"
+elif [ "$valthr" -eq "10" ] ; then
+        echo "Compilation PASS , Memory leaks:FAIL , thred race : PASS"
 	cd $currentLocation
         exit 2
 else
-        echo "Memory leaks :PASS , thred race :PASS"
+        echo "Compilation PASS , Memory leaks :PASS , thred race :PASS"
 	cd $currentLocation
         exit 0
 fi
